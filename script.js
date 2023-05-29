6149513882d40bb533b94d65664b8db47e63a055
samir <samir.comas1@gmail.com>
2023-05-29 12:15:55 +0200

    añadiendo texto  script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hola me falto este mensaje");
+function hacerComida(){
+    let esto = document.getElementById("resr").valueAsNumn;
+
+    let asr 
+    let comida = comida()
+}
+
+function comida(){
+    let comida2;
+
+    return comida2;f 
+
+}
